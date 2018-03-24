26aae669600f6c3b520c28d5638df299d8e44fed
Gaurav Tamhan <gaurav.tamhan@gmail.com>
2018-03-23 22:57:52 -0400

    cross origin change

--- js/main.js
+++ js/main.js
@@ -41,7 +41,7 @@ function displayDayAndTime() {
 
 
 function loadWeatherData(city) {
-    let api = "http://api.openweathermap.org/data/2.5/weather?q=";
+    let api = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?q=";
     city = encodeURIComponent(city.trim());
     let units = "&units=imperial"
     let key = "&appid=c2e34159fe50dfa33d4dfc1b89a3d3fc"
